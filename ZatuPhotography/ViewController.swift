b0cbd71137b21c4eeeb061c5e4b1278d20a920e5
anis <khoirunnisa.rizky.n@>
2019-04-22 06:42:35 +0700

    Initial Commit

---
+++ ZatuPhotography/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZatuPhotography
+//
+//  Created by khoirunnisa' rizky noor fatimah on 22/04/19.
+//  Copyright © 2019 khoirunnisa' rizky noor fatimah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
